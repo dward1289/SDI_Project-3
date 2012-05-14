4cc0d131c71de92bdabfd0d11674ade93f3d1dfc
Devona Ward <dward1289@fullsail.edu>
2012-05-14 13:34:35 -0400

    JSON file added.

---
+++ json.js
@@ -0,0 +1,65 @@
+var json = {
+	"criminalList": {
+		"1408": {
+			"name": "Smith Mills",
+			"age": 24,
+			"crime": "Break-in",
+			"years": 4,
+			"wanted": "Robbery",
+			"warrants": 12
+	},
+	
+		"1567": {
+			"name": "Tommy Connor",
+			"age": 26,
+			"crime": "Accessory",
+			"years": 1,
+			"wanted": "Robbery",
+			"warrants": 2
+	},
+		
+		"1778": {
+			"name": "James Lim",
+			"age": 28,
+			"crime": "Car Theft",
+			"years": 1,
+			"wanted": "Car Theft and Robbery",
+			"warrants": 3
+		}
+	}
+};
+
+
+var json2 = {
+	"criminalList": [
+	{
+		"id": "1408",
+		"name": "Smith Mills",
+		"age": 24,
+		"crime": "Break-in",
+		"years": 4,
+		"wanted": "Robbery",
+		"warrants": 12
+	},
+	
+	{
+		"id": "1567",
+		"name": "Tommy Connor",
+		"age": 26,
+		"crime": "Accessory",
+		"years": 1,
+		"wanted": "Robbery",
+		"warrants": 2
+	},
+	
+	{
+		"id": "1778",
+		"name": "James Lim",
+		"age": 28,
+		"crime": "Car Theft",
+		"years": 1,
+		"wanted": "Car Theft and Robbery",
+		"warrants": 3
+		}
+	]
+};
--- ward_devona_assignment3.js
+++ ward_devona_assignment3.js
@@ -1,1 +1,32 @@
-alert("JavaScript works!");
+var areas = ["Rooftop","Bank","Vault","Safe-house"];
+var spotTime = [5,5,15,20];
+var driver = {
+	name: "James"
+	};
+	
+var lookOut = {
+	name: "Tommy"
+	};
+	
+var lockMan = {
+	name: "Smith"
+	};
+	
+
+var intro = function () {
+console.log("We have a problem! A robbery is going to happen soon.");
+console.log(lockMan.name + ", " + lookOut.name + ", and " + driver.name + " are looking to do some damage.");
+};
+
+var handleData = function (json) {
+	for (var i = 0; i < json.criminalList.length; i++){
+	var crimeList = json.criminalList[i];
+	console.log("ID: " + crimeList.id + ", Name: " + crimeList.name + ", Age: " + crimeList.age + ", Crime: " + crimeList.crime + ", Time Spent in Lockup: " + crimeList.years + ", Wanted for: " + crimeList.wanted + ", Warrants: " + crimeList.warrants);
+	};
+};
+	
+intro();
+
+console.log("Here is the list of the criminals you wanted, Boss.");
+
+handleData(json2);
